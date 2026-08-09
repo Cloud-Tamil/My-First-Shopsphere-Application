c7a65225be5cf89d37540ac61e6f99949a3f59e8
Cloud Engineer <sktamilvb@gmail.com>
2026-08-09 13:51:03 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_version = ">= 1.9.0"
 
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.57"
+    }
+
+  }
+}
+
+
+provider "aws" {
+
+  region = var.aws_region
+
+}
