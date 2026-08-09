a1d42cbe80774506a069dba44bd75693a2a7a8de
Cloud Engineer <sktamilvb@gmail.com>
2026-08-09 13:52:21 +0530

    Update main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,1 +1,67 @@
+resource "aws_ecr_repository" "frontend" {
 
+  name = "${var.project_name}/frontend"
+
+  image_tag_mutability = "MUTABLE"
+
+
+  image_scanning_configuration {
+
+    scan_on_push = true
+
+  }
+
+
+  tags = {
+
+    Project = var.project_name
+
+  }
+
+}
+
+
+resource "aws_ecr_repository" "backend" {
+
+  name = "${var.project_name}/backend"
+
+  image_tag_mutability = "MUTABLE"
+
+
+  image_scanning_configuration {
+
+    scan_on_push = true
+
+  }
+
+
+  tags = {
+
+    Project = var.project_name
+
+  }
+
+}
+
+
+variable "project_name" {
+
+  type = string
+
+}
+
+
+output "frontend_repository_url" {
+
+  value =
+    aws_ecr_repository.frontend.repository_url
+
+}
+
+
+output "backend_repository_url" {
+
+  value =
+    aws_ecr_repository.backend.repository_url
+
+}
