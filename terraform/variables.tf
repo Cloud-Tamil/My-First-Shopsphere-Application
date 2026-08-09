69fae7c500540e8c005fb0742bcc65f7a0663c2a
Cloud Engineer <sktamilvb@gmail.com>
2026-08-09 13:51:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,63 @@
+variable "aws_region" {
 
+  description = "AWS region"
+
+  type = string
+
+  default = "ap-south-1"
+
+}
+
+
+variable "project_name" {
+
+  description = "Project name"
+
+  type = string
+
+  default = "shopsphere"
+
+}
+
+
+variable "environment" {
+
+  description = "Environment"
+
+  type = string
+
+  default = "dev"
+
+}
+
+
+variable "vpc_cidr" {
+
+  description = "VPC CIDR"
+
+  type = string
+
+  default = "10.0.0.0/16"
+
+}
+
+
+variable "db_username" {
+
+  description = "RDS username"
+
+  type = string
+
+  default = "shopsphere"
+
+}
+
+
+variable "db_password" {
+
+  description = "RDS password"
+
+  type = string
+
+  sensitive = true
+}
