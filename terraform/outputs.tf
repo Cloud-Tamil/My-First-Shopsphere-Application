48aefff05ee69423c9acb145bd1d86751faf804e
Cloud Engineer <sktamilvb@gmail.com>
2026-08-09 13:54:24 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,54 @@
+output "vpc_id" {
 
+  value =
+    module.vpc.vpc_id
+
+}
+
+
+output "eks_cluster_name" {
+
+  value =
+    module.eks.cluster_name
+
+}
+
+
+output "eks_cluster_endpoint" {
+
+  value =
+    module.eks.cluster_endpoint
+
+}
+
+
+output "frontend_ecr" {
+
+  value =
+    module.ecr.frontend_repository_url
+
+}
+
+
+output "backend_ecr" {
+
+  value =
+    module.ecr.backend_repository_url
+
+}
+
+
+output "rds_endpoint" {
+
+  value =
+    module.rds.db_endpoint
+
+}
+
+
+output "s3_bucket" {
+
+  value =
+    module.s3.bucket_name
+
+}
